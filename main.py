9ed94b6c114d17c07a14ceb63fcf33357d9b9ce1
dborodin836 <dborodin836@gmail.com>
2022-07-09 17:18:13 +0300

    tmp

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template
 
-from parsers import print_djinni_data
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return print_djinni_data()
+    return render_template("home.html")
 
 
 if __name__ == '__main__':
